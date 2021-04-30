b52e0e47c8e96638f83bd50e12fc79a3118e84b9
Carla Bruckmann <72099071+CarlaBruckmann@users.noreply.github.com>
2021-04-29 21:03:59 -0300

    refactor(app) update menu
    Added houdini effect

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+// menu
 function initMenu(){
     let icon = document.querySelector('.icon');
     let menu = document.querySelector('.menu');
@@ -15,3 +17,17 @@ function initMenu(){
 }
 
 initMenu();
+
+
+// houdini
+(async function() {
+    // Polyfill for older browsers//
+if (CSS["paintWorklet"] === undefined)
+  await import(
+    "https://unpkg.com/css-paint-polyfill@next/dist/css-paint-polyfill.js"
+  );
+// Paint Worklet https://github.com/hazmi/curved-line //
+CSS.paintWorklet.addModule(
+  "https://unpkg.com/curved-line@1.0.0/curved-line.js"
+);
+})();
